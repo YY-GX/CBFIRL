3999b237d9feba40d3a840df3b0e2fe470a0f0d2
YY-GX <yyang9923@gmail.com>
2022-10-22 16:08:16 -0400

    add some shells. server run 8 obs

--- shell_scripts/run_trpo_cbf.sh
+++ shell_scripts/run_trpo_cbf.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-w_cbf=(10000 7500 5000 2500)
-entro_coeff=(0.001 0.01)
-generator_iter=(10)
-max_kl=(0.01 0.1)
-ent_methods=("regularized")
+#w_cbf=(10000 7500 5000 2500)
+#entro_coeff=(0.001 0.01)
+#generator_iter=(10)
+#max_kl=(0.01 0.1)
+#ent_methods=("regularized")
 
 #w_cbf=(10000 1000 100)
 #entro_coeff=(0.001)
@@ -13,11 +13,12 @@ ent_methods=("regularized")
 #max_kl=(0.01)
 #ent_methods=("regularized")
 
-w_cbf=(10000)
-entro_coeff=(0.01)
+w_cbf=(10000 7500)
+entro_coeff=(0.001)
 generator_iter=(10)
 max_kl=(0.01)
 ent_methods=("regularized")
+seeds=(1 2 3 4 5 6 7 8 9 10)
 
 
 i=1
@@ -34,23 +35,26 @@ for w in "${w_cbf[@]}";
               do
                 for ent_m in "${ent_methods[@]}";
                   do
-                    mainfolder="${FOLDER}repro_3_$i"
-                    python scripts/train_trpo_cbf.py \
-                     --seed 10 \
-                     --cbf_weight $w \
-                     --epoch_num 76 \
-                     --cbf_pth "data/new_comb_new_demo/cbf_posx_posy/cbf"\
-                     --restore_pth "data/trpo_cbf/pretrain_airl/airl"\
-                     --is_restore 1 \
-                     --is_freeze_discriminator 1 \
-                     --is_auto_tuning 1 \
-                     --generator_train_itrs $num_gen \
-                     --policy_ent_coeff $entro \
-                     --max_kl_step $m_kl \
-                     --ent_method $ent_m \
-                     --is_use_two_step 1 \
-                     --log_pth "${mainfolder}/log" --share_pth "${mainfolder}/share" --airl_pth "${mainfolder}/airl"
-                    i=$(($i+1))
+                    for s in "${seeds[@]}";
+                      do
+                        mainfolder="${FOLDER}cbf_airl_1_5_try2_$i"
+                        python scripts/train_trpo_cbf.py \
+                         --seed $s \
+                         --cbf_weight $w \
+                         --epoch_num 75 \
+                         --cbf_pth "data/new_comb_new_demo/cbf_posx_posy/cbf"\
+                         --restore_pth "data/trpo_cbf/pretrain_airl/airl"\
+                         --is_restore 1 \
+                         --is_freeze_discriminator 1 \
+                         --is_auto_tuning 1 \
+                         --generator_train_itrs $num_gen \
+                         --policy_ent_coeff $entro \
+                         --max_kl_step $m_kl \
+                         --ent_method $ent_m \
+                         --is_use_two_step 1 \
+                         --log_pth "${mainfolder}/log" --share_pth "${mainfolder}/share" --airl_pth "${mainfolder}/airl"
+                        i=$(($i+1))
+                      done
                   done
               done
           done
